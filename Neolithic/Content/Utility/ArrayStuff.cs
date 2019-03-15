90d3bc2db2bb668eeba07d881cbdcd6435a61d9e
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-03-15 16:44:24 -0400

    prevent index from reaching numbers larger than array size

--- Neolithic/Content/Utility/ArrayStuff.cs
+++ Neolithic/Content/Utility/ArrayStuff.cs
@@ -10,7 +10,8 @@ namespace TheNeolithicMod
     {
         public static T Next<T>(this T[] array, ref uint index)
         {
-            return array[++index % array.Length];
+            index = (uint)(++index % array.Length);
+            return array[index];
         }
     }
 }
