85047a59e27a397a214945be056a17686e3c33f1
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-03-20 05:46:45 -0400

    put hand animations in utility class.

---
+++ Neolithic/Content/Utility/HandAnimations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+
+namespace TheNeolithicMod
+{
+    public static class HandAnimations
+    {
+        public static bool Hit(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
+        {
+            if (world.Side == EnumAppSide.Client)
+            {
+                ModelTransform tf = new ModelTransform();
+
+                tf.EnsureDefaultValues();
+
+                tf.Origin.Set(0f, 0f, 0f);
+                tf.Translation.Set(0f, 0f, 0f);
+
+                tf.Rotation.X -= (float)Math.Sin(secondsUsed * 6) * 90;
+
+                byPlayer.Entity.Controls.UsingHeldItemTransformAfter = tf;
+                return tf.Rotation.X > -80;
+            }
+            return true;
+        }
+    }
+}
