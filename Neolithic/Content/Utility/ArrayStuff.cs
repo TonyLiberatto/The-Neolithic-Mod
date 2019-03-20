30dd5488cc0df8eb3b6d35db317f886819817dcd
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-03-19 20:03:20 -0400

    way to figure out what wood type a block uses

--- Neolithic/Content/Utility/ArrayStuff.cs
+++ Neolithic/Content/Utility/ArrayStuff.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vintagestory.API.Common;
 
 namespace TheNeolithicMod
 {
     public static class ArrayStuff
     {
+        static readonly string[] woodtypes = new string[] {
+            "birch",
+            "oak",
+            "maple",
+            "pine",
+            "acacia",
+            "kapok",
+        };
+
         public static T Next<T>(this T[] array, ref uint index)
         {
             index = (uint)(++index % array.Length);
             return array[index];
+        }
+
+        public static bool MatchingWood(this AssetLocation asset, ref string woodtype)
+        {
+            foreach (string a in woodtypes)
+            {
+                if (asset.ToString().Contains(a))
+                {
+                    woodtype = a;
+                    return true;
+                }
+            }
+            woodtype = null;
+            return false;
         }
     }
 }
