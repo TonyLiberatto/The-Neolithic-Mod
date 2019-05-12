8bf4ad5eb5b556044da3ce795e9488779b620d53
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-05-12 15:25:29 -0400

    Fix adze crash

--- Neolithic/Content/Item/ItemAdze.cs
+++ Neolithic/Content/Item/ItemAdze.cs
@@ -4,16 +4,12 @@ using Vintagestory.GameContent;
 
 namespace TheNeolithicMod
 {
-    public class ItemAdze : ItemChisel
-    {
-        public override void OnLoaded(ICoreAPI api)
-        {
-            base.OnLoaded(api);
-            canMicroChisel = true;
-        }
-
+    public class ItemAdze : ItemChiselFix
+	{
         public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
         {
+			if (blockSel == null) return;
+
             Block block = byEntity.World.BlockAccessor.GetBlock(blockSel.Position);
             if (block.BlockMaterial != EnumBlockMaterial.Wood) return;
             base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
