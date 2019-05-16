4d7d0bfd476b928170cb4420df740a43f809f63d
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-05-15 23:58:16 -0400

    declare most variables in the OnLoaded method for fixedstairs

--- Neolithic/Content/Block/FixedStairs.cs
+++ Neolithic/Content/Block/FixedStairs.cs
@@ -11,21 +11,40 @@ namespace TheNeolithicMod
 {
     public class FixedStairs : BlockStairs
     {
-        Block block;
+		string type, material, updown, outside, inside;
+		Block north, south, east, west;
+		bool side, corner;
 
-        public override void OnNeighourBlockChange(IWorldAccessor world, BlockPos pos, BlockPos neibpos)
+		public override void OnLoaded(ICoreAPI api)
+		{
+			base.OnLoaded(api);
+
+			type = FirstCodePart(2);
+			material = FirstCodePart(3);
+			updown = LastCodePart(1);
+			outside = "stair-corner-" + type + "-" + material + "-" + "outside" + "-" + updown;
+			inside = "stair-corner-" + type + "-" + material + "-" + "inside" + "-" + updown;
+			north = new AssetLocation("stair-side-" + type + "-" + material + "-" + updown + "-north").GetBlock(api);
+			south = new AssetLocation("stair-side-" + type + "-" + material + "-" + updown + "-south").GetBlock(api);
+			east = new AssetLocation("stair-side-" + type + "-" + material + "-" + updown + "-east").GetBlock(api);
+			west = new AssetLocation("stair-side-" + type + "-" + material + "-" + updown + "-west").GetBlock(api);
+
+			side = WildCardMatch(new AssetLocation("*stair-side*"));
+			corner = WildCardMatch(new AssetLocation("*stair-corner*"));
+		}
+
+		public override void OnNeighourBlockChange(IWorldAccessor world, BlockPos pos, BlockPos neibpos)
         {
-            block = world.BlockAccessor.GetBlock(pos);
-            Block nBlock = world.BlockAccessor.GetBlock(neibpos);
+            Block nBlock = neibpos.GetBlock(world);
             if (!nBlock.WildCardMatch(new AssetLocation("stair*")) && !nBlock.WildCardMatch(new AssetLocation("air*"))) return;
 
             AssetLocation[] cardinal = GetCardinal(world, pos);
 
-            if (block.WildCardMatch(new AssetLocation("*stair-side*")))
+            if (side)
             {
                 StairsCheck(world, pos, cardinal);
             }
-            else if (block.WildCardMatch(new AssetLocation("*stair-corner*")))
+            else if (corner)
             {
                 CornersCheck(world, pos, cardinal);
             }
@@ -34,10 +53,9 @@ namespace TheNeolithicMod
         public override void OnBlockPlaced(IWorldAccessor world, BlockPos blockPos, ItemStack byItemStack = null)
         {
             base.OnBlockPlaced(world, blockPos, byItemStack);
-            block = world.BlockAccessor.GetBlock(blockPos);
             AssetLocation[] cardinal = GetCardinal(world, blockPos);
 
-            if (block.WildCardMatch(new AssetLocation("*stair-side*")))
+            if (side)
             {
                 StairsCheck(world, blockPos, cardinal);
             }
@@ -57,8 +75,6 @@ namespace TheNeolithicMod
         {
 
             var bA = world.BlockAccessor;
-            string type = FirstCodePart(2);
-            string material = FirstCodePart(3);
 
             if (!cardinal.Any(val => bA.GetBlock(val).WildCardMatch(new AssetLocation("*stair-side*")))) return;
             string cardinalN = bA.GetBlock(cardinal[0]).LastCodePart();
@@ -66,52 +82,49 @@ namespace TheNeolithicMod
             string cardinalE = bA.GetBlock(cardinal[2]).LastCodePart();
             string cardinalW = bA.GetBlock(cardinal[3]).LastCodePart();
 
-            string updown = world.BlockAccessor.GetBlock(pos).LastCodePart(1);
-            string baseStringO = "stair-corner-" + type + "-" + material + "-" + "outside" + "-" + updown;
-            string baseStringI = "stair-corner-" + type + "-" + material + "-" + "inside" + "-" + updown;
 
             if (cardinalW == "east")
             {
                 if (cardinalN == "north")
                 {
-                    bA.SetBlock(bA.GetBlock(new AssetLocation(baseStringO + "-northeast")).BlockId, pos);
+                    bA.SetBlock(bA.GetBlock(new AssetLocation(outside + "-northeast")).BlockId, pos);
                 }
                 else if (cardinalS == "south")
                 {
-                    bA.SetBlock(bA.GetBlock(new AssetLocation(baseStringI + "-southeast")).BlockId, pos);
+                    bA.SetBlock(bA.GetBlock(new AssetLocation(inside + "-southeast")).BlockId, pos);
                 }
             }
             else if (cardinalW == "west")
             {
                 if (cardinalN == "south")
                 {
-                    bA.SetBlock(bA.GetBlock(new AssetLocation(baseStringI + "-southwest")).BlockId, pos);
+                    bA.SetBlock(bA.GetBlock(new AssetLocation(inside + "-southwest")).BlockId, pos);
                 }
                 else if (cardinalS == "north")
                 {
-                    bA.SetBlock(bA.GetBlock(new AssetLocation(baseStringO + "-northwest")).BlockId, pos);
+                    bA.SetBlock(bA.GetBlock(new AssetLocation(outside + "-northwest")).BlockId, pos);
                 }
             }
             else if (cardinalE == "east")
             {
                 if (cardinalN == "south")
                 {
-                    bA.SetBlock(bA.GetBlock(new AssetLocation(baseStringO + "-southeast")).BlockId, pos);
+                    bA.SetBlock(bA.GetBlock(new AssetLocation(outside + "-southeast")).BlockId, pos);
                 }
                 else if (cardinalS == "north")
                 {
-                    bA.SetBlock(bA.GetBlock(new AssetLocation(baseStringI + "-northeast")).BlockId, pos);
+                    bA.SetBlock(bA.GetBlock(new AssetLocation(inside + "-northeast")).BlockId, pos);
                 }
             }
             else if (cardinalE == "west")
             {
                 if (cardinalN == "north")
                 {
-                    bA.SetBlock(bA.GetBlock(new AssetLocation(baseStringI + "-northwest")).BlockId, pos);
+                    bA.SetBlock(bA.GetBlock(new AssetLocation(inside + "-northwest")).BlockId, pos);
                 }
                 else if (cardinalS == "south")
                 {
-                    bA.SetBlock(bA.GetBlock(new AssetLocation(baseStringO + "-southwest")).BlockId, pos);
+                    bA.SetBlock(bA.GetBlock(new AssetLocation(outside + "-southwest")).BlockId, pos);
                 }
             }
         }
@@ -119,96 +132,89 @@ namespace TheNeolithicMod
         public void CornersCheck(IWorldAccessor world, BlockPos pos, AssetLocation[] cardinal)
         {
             var bA = world.BlockAccessor;
-            string type = FirstCodePart(2);
-            string material = FirstCodePart(3);
-            string updown = LastCodePart(1);
-            Block north = bA.GetBlock(new AssetLocation("stair-side-" + type + "-" + material + "-" + updown + "-north"));
-            Block south = bA.GetBlock(new AssetLocation("stair-side-" + type + "-" + material + "-" + updown + "-south"));
-            Block east = bA.GetBlock(new AssetLocation("stair-side-" + type + "-" + material + "-" + updown + "-east"));
-            Block west = bA.GetBlock(new AssetLocation("stair-side-" + type + "-" + material + "-" + updown + "-west"));
-            string cardinalN = bA.GetBlock(cardinal[0]).LastCodePart();
+
+			string cardinalN = bA.GetBlock(cardinal[0]).LastCodePart();
             string cardinalS = bA.GetBlock(cardinal[1]).LastCodePart();
             string cardinalE = bA.GetBlock(cardinal[2]).LastCodePart();
             string cardinalW = bA.GetBlock(cardinal[3]).LastCodePart();
 
-
-            if (LastCodePart() == "northeast")
-            {
-                if (cardinalN == "north")
-                {
-                    bA.SetBlock(north.BlockId, pos);
-                }
-                else if (cardinalE == "east")
-                {
-                    bA.SetBlock(east.BlockId, pos);
-                }
-                else if (cardinalW == "east")
-                {
-                    bA.SetBlock(east.BlockId, pos);
-                }
-                else if (cardinalS == "north")
-                {
-                    bA.SetBlock(north.BlockId, pos);
-                }
-
-            }
-            else if (LastCodePart() == "southwest")
-            {
-                if (cardinalS == "south")
-                {
-                    bA.SetBlock(south.BlockId, pos);
-                }
-                else if (cardinalE == "west")
-                {
-                    bA.SetBlock(west.BlockId, pos);
-                }
-                else if (cardinalN == "south")
-                {
-                    bA.SetBlock(south.BlockId, pos);
-                }
-                else if (cardinalW == "west")
-                {
-                    bA.SetBlock(west.BlockId, pos);
-                }
-            }
-            else if (LastCodePart() == "southeast")
-            {
-                if (cardinalN == "south")
-                {
-                    bA.SetBlock(south.BlockId, pos);
-                }
-                else if (cardinalE == "east")
-                {
-                    bA.SetBlock(east.BlockId, pos);
-                }
-                else if (cardinalS == "south")
-                {
-                    bA.SetBlock(south.BlockId, pos);
-                }
-                else if (cardinalW == "east")
-                {
-                    bA.SetBlock(east.BlockId, pos);
-                }
-            }
-            else if (LastCodePart() == "northwest")
-            {
-                if (cardinalW == "west")
-                {
-                    bA.SetBlock(west.BlockId, pos);
-                }
-                else if (cardinalS == "north")
-                {
-                    bA.SetBlock(north.BlockId, pos);
-                }
-                else if (cardinalE == "west")
-                {
-                    bA.SetBlock(west.BlockId, pos);
-                }
-                else if (cardinalN == "north")
-                {
-                    bA.SetBlock(north.BlockId, pos);
-                }
-            }
+			switch (LastCodePart())
+			{
+				case "northeast":
+					if (cardinalN == "north")
+					{
+						bA.SetBlock(north.BlockId, pos);
+					}
+					else if (cardinalE == "east")
+					{
+						bA.SetBlock(east.BlockId, pos);
+					}
+					else if (cardinalW == "east")
+					{
+						bA.SetBlock(east.BlockId, pos);
+					}
+					else if (cardinalS == "north")
+					{
+						bA.SetBlock(north.BlockId, pos);
+					}
+					break;
+				case "southwest":
+					if (cardinalS == "south")
+					{
+						bA.SetBlock(south.BlockId, pos);
+					}
+					else if (cardinalE == "west")
+					{
+						bA.SetBlock(west.BlockId, pos);
+					}
+					else if (cardinalN == "south")
+					{
+						bA.SetBlock(south.BlockId, pos);
+					}
+					else if (cardinalW == "west")
+					{
+						bA.SetBlock(west.BlockId, pos);
+					}
+					break;
+				case "southeast":
+					if (cardinalN == "south")
+					{
+						bA.SetBlock(south.BlockId, pos);
+					}
+					else if (cardinalE == "east")
+					{
+						bA.SetBlock(east.BlockId, pos);
+					}
+					else if (cardinalS == "south")
+					{
+						bA.SetBlock(south.BlockId, pos);
+					}
+					else if (cardinalW == "east")
+					{
+						bA.SetBlock(east.BlockId, pos);
+					}
+					break;
+				case "northwest":
+					if (cardinalW == "west")
+					{
+						bA.SetBlock(west.BlockId, pos);
+					}
+					else if (cardinalS == "north")
+					{
+						bA.SetBlock(north.BlockId, pos);
+					}
+					else if (cardinalE == "west")
+					{
+						bA.SetBlock(west.BlockId, pos);
+					}
+					else if (cardinalN == "north")
+					{
+						bA.SetBlock(north.BlockId, pos);
+					}
+					break;
+				default:
+					break;
+			}
         }
 
         public static AssetLocation[] GetCardinal(IWorldAccessor world, BlockPos pos)
