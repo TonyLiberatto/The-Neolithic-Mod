4662a2b79c0c24288358bb2ae4c8ae111053ec44
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-08-03 12:19:57 -0400

    Add returnBlock to attributes for BlockToolMold

---
+++ Neolithic/Systems/ReturnBlockTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace TheNeolithicMod
+{
+    class BlockToolMoldReturnBlock : BlockToolMold
+    {
+        public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
+        {
+            if (blockSel == null) return false;
+            bool ok = (world.BlockAccessor.GetBlockEntity(blockSel.Position) as BlockEntityToolMold)?.OnPlayerInteract(byPlayer, blockSel.Face, blockSel.HitPosition) != null ? true : false;
+            if (ok)
+            {
+                if (Attributes["returnBlock"].Exists)
+                {
+                    world.BlockAccessor.ExchangeBlock(Attributes["returnBlock"].AsString().WithDomain().ToBlock(api).BlockId, blockSel.Position);
+                }
+            }
+            return ok;
+        }
+    }
+}
