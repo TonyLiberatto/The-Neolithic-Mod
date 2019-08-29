b10d53d8c1b372b7d0e20ccc3949a9c505cb237f
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-08-28 22:31:35 -0400

    Add seawater

---
+++ Neolithic/Content/WorldGen/GenLakes.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.Client.NoObf;
+using Vintagestory.ServerMods;
+using Vintagestory.ServerMods.NoObf;
+
+namespace TheNeolithicMod
+{
+    public class DisableVanillaLakes : ModSystem
+    {
+        ICoreServerAPI api;
+        public override double ExecuteOrder() => 0;
+        public override bool ShouldLoad(EnumAppSide forSide) => forSide.IsServer();
+        long id;
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            this.api = api;
+            api.Event.ServerRunPhase(EnumServerRunPhase.LoadGamePre, ChangeLakeWater);
+        }
+
+        public void ChangeLakeWater()
+        {
+            NeolithicGlobalConfig config = api.Assets.Get("worldgen/global.json").ToObject<NeolithicGlobalConfig>();
+            config.SetApi(api);
+            //api.ModLoader.GetModSystem<GenLakes>()
+            id = api.Event.RegisterGameTickListener(dt => 
+            {
+                if (api.ModLoader.GetModSystem<GenLakes>().GlobalConfig != null)
+                {
+                    api.ModLoader.GetModSystem<GenLakes>().GlobalConfig.waterBlockCode = config.lakeWaterBlockCode;
+                    api.ModLoader.GetModSystem<GenLakes>().GlobalConfig.waterBlockId = config.lakeWaterBlockId;
+                    api.ModLoader.GetModSystem<GenLakes>().GlobalConfig.lakeIceBlockCode = config.lakeIceBlockCode;
+                    api.ModLoader.GetModSystem<GenLakes>().GlobalConfig.lakeIceBlockId = config.lakeIceBlockId;
+                    api.Event.UnregisterGameTickListener(id);
+                }
+            }, 30);
+        }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class NeolithicGlobalConfig
+    {
+        [JsonProperty]
+        public AssetLocation waterBlockCode;
+
+        [JsonProperty]
+        public AssetLocation lakeWaterBlockCode;
+
+        [JsonProperty]
+        public AssetLocation lakeIceBlockCode;
+
+        [JsonProperty]
+        public AssetLocation glacierIceBlockCode;
+
+        [JsonProperty]
+        public AssetLocation lavaBlockCode;
+
+        [JsonProperty]
+        public AssetLocation basaltBlockCode;
+
+        [JsonProperty]
+        public AssetLocation mantleBlockCode;
+
+        [JsonProperty]
+        public AssetLocation defaultRockCode;
+
+        private ICoreAPI api;
+
+        public int waterBlockId { get => waterBlockCode.GetID(api); }
+        public int lakeWaterBlockId { get => lakeWaterBlockCode.GetID(api); }
+        public int lakeIceBlockId { get => lakeIceBlockCode.GetID(api); }
+        public int glacierIceBlockId { get => glacierIceBlockCode.GetID(api); }
+        public int lavaBlockId { get => lavaBlockCode.GetID(api); }
+        public int basaltBlockId { get => basaltBlockCode.GetID(api); }
+        public int mantleBlockId { get => mantleBlockCode.GetID(api); }
+        public int defaultRockId { get => defaultRockCode.GetID(api); }
+
+        public void SetApi(ICoreAPI api)
+        {
+            this.api = api;
+        }
+    }
+}
