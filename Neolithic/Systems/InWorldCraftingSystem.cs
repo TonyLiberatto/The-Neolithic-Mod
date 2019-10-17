5fd08bd92656ba4299a46e4ddeb57ea05b2de8fa
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2019-10-17 02:19:34 -0400

    log serialized data, throw exception

--- Neolithic/Systems/InWorldCraftingSystem.cs
+++ Neolithic/Systems/InWorldCraftingSystem.cs
@@ -77,7 +77,8 @@ namespace Neolithic
                     }
                     catch (Exception ex)
                     {
-                        api.World.Logger.Error("Exception thrown while receiving an In World Recipe packet: {0}", ex);
+                        api.World.Logger.Error("Exception thrown while receiving an In World Recipe packet: {0}, Data: {1}", ex, h.SerializedData);
+                        throw ex;
                     }
                 }
             });
